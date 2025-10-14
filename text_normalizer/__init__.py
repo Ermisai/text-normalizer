b375baa6721fb4cfa4cdf79f84e7f2a23a1ed0b3
Vittor Lima Nome <vittorlima@discente.ufg.br>
2025-10-14 12:42:41 +0000

    Primeira versão da lib text_normalizer

---
+++ text_normalizer/__init__.py
@@ -0,0 +1,19 @@
+from .normalizer import (
+    normalize_text,
+    normalize_currency,
+    normalize_dates,
+    normalize_time,
+    normalize_numbers,
+    normalize_numeric_sequences,
+    remove_non_alphanumeric
+)
+
+__all__ = [
+    "normalize_text",
+    "normalize_currency",
+    "normalize_dates",
+    "normalize_time",
+    "normalize_numbers",
+    "normalize_numeric_sequences",
+    "remove_non_alphanumeric"
+]
